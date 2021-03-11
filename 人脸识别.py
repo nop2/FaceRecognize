f0d7b5744295fe3bb7190a33914bc8bd0c07c791
ErrorXin <42483111+2845963383@users.noreply.github.com>
2021-03-11 14:38:18 +0800

    默认的

--- 人脸识别.py
+++ 人脸识别.py
@@ -4,94 +4,113 @@ import numpy as np
 import pandas as pd
 import torch
 from facenet_pytorch import MTCNN, InceptionResnetV1
-
-# 选择设备
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Running on device: {}'.format(device))
-
-# 读取训练好的人脸特征数据
-data = pd.read_csv('face_data.csv')
-x = data.drop(columns=['label'])
-y = data['label']
+from PIL import Image, ImageDraw, ImageFont
 
 
-# 根据特征向量识别人脸，使用欧氏距离，如果距离大于1则认为识别失败
-def recognize(v):
-    dis = np.sqrt(sum((v[0] - x.iloc[0]) ** 2))
-    name = y[0]
+# 人脸识别器
+class FaceRecognizer:
+    # 初始化，加载数据
+    def __init__(self):
+        # 选择设备
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        print('Running on device: {}'.format(self.device))
 
-    for i in range(1, x.shape[0]):
-        temp_dis = np.sqrt(sum((v[0] - x.iloc[i]) ** 2))
-        if temp_dis < dis:
-            dis = temp_dis
-            name = y[i]
+        # 读取训练好的人脸特征数据
+        self.data = pd.read_csv('face_feature.csv')
+        self.x = self.data.drop(columns=['label'])
+        self.y = self.data['label']
 
-    return name, dis
+        # 字体文件，用于在图片上正确显示中文
+        self.font = ImageFont.truetype('simsun.ttc', size=30)
 
+        # 加载训练好的KNN分类器模型
+        self.knn_model = joblib.load('knn_model.pkl')
 
-def test():
-    # mtcnn检测人脸位置
-    mtcnn = MTCNN(device=device, keep_all=True)
-    # 用于生成人脸512维特征向量
-    resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+    # 根据特征向量识别人脸，使用欧氏距离，如果距离大于1则认为识别失败
+    def _recognize(self, v):
+        dis = np.sqrt(sum((v[0] - self.x.iloc[0]) ** 2))
+        name = self.y[0]
 
-    # 加载训练好的KNN分类器模型
-    knn = joblib.load('knn_model.pkl')
+        for i in range(1, self.x.shape[0]):
+            temp_dis = np.sqrt(sum((v[0] - self.x.iloc[i]) ** 2))
+            if temp_dis < dis:
+                dis = temp_dis
+                name = self.y[i]
 
-    # 初始化视频窗口
-    windows_name = 'face'
-    cv2.namedWindow(windows_name)
-    cap = cv2.VideoCapture(0)
+        return name, dis
 
-    count = 0
-    while True:
-        # 从摄像头读取一帧图像
-        success, image = cap.read()
-        if not success:
-            break
+    # 人脸识别主函数
+    def start_recognize(self):
+        # mtcnn检测人脸位置
+        mtcnn = MTCNN(device=self.device, keep_all=True)
+        # 用于生成人脸512维特征向量
+        resnet = InceptionResnetV1(pretrained='vggface2').eval().to(self.device)
 
-        # 检测人脸位置
-        boxes, probs = mtcnn.detect(image)
-        if boxes is not None:
-            for box, prob in zip(boxes, probs):
-                # 设置人脸检测阈值
-                if prob < 0.9:
-                    continue
+        # 初始化视频窗口
+        windows_name = 'face'
+        cv2.namedWindow(windows_name)
+        cap = cv2.VideoCapture(0)
 
-                x1, y1, x2, y2 = [int(p) for p in box]
-                # 框出人脸位置
-                cv2.rectangle(image, (x1 - 10, y1 - 10), (x2 + 10, y2 + 10), color=(0, 255, 0), thickness=2)
-                # cv2.putText(image, str(round(prob, 3)), (x1, y1 - 30), cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
+        while True:
+            # 从摄像头读取一帧图像
+            success, image = cap.read()
+            if not success:
+                break
 
-                # 导出人脸图像
-                face = mtcnn.extract(image, [box], None).to(device)
-                # 生成512维特征向量
-                embeddings = resnet(face).detach().cpu().numpy()
-                # KNN预测
-                name_knn = knn.predict(embeddings)
+            img = Image.fromarray(image)
+            draw = ImageDraw.Draw(img)
 
-                # 获得预测姓名和距离
-                _, dis = recognize(embeddings)
-                # 如果距离过大则认为识别失败
-                if dis > 0.9:
-                    cv2.putText(image, f'unknown', (x1 - 20, y1 - 20), cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
-                else:
-                    # 将识别出的名字和距离显示在图像上
-                    cv2.putText(image, f'{name_knn[0]}({round(dis, 2)})', (x1 - 20, y1 - 20),
-                                cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
+            # 检测人脸位置
+            boxes, probs = mtcnn.detect(image)
+            if boxes is not None:
+                for box, prob in zip(boxes, probs):
+                    # 设置人脸检测阈值
+                    if prob < 0.9:
+                        continue
 
-        # 显示处理后的图片
-        cv2.imshow(windows_name, image)
+                    x1, y1, x2, y2 = [int(p) for p in box]
+                    # 框出人脸位置
+                    draw.rectangle((x1, y1, x2, y2), outline=(0, 255, 0), width=2)
+                    # cv2.rectangle(image, (x1 - 10, y1 - 10), (x2 + 10, y2 + 10), color=(0, 255, 0), thickness=2)
+                    # cv2.putText(image, str(round(prob, 3)), (x1, y1 - 30), cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
 
-        # 保持窗口
-        key = cv2.waitKey(1)
-        if key & 0xff == 27:
-            break
+                    # 导出人脸图像
+                    face = mtcnn.extract(image, [box], None).to(self.device)
+                    # 生成512维特征向量
+                    embeddings = resnet(face).detach().cpu().numpy()
+                    # KNN预测
+                    name_knn = self.knn_model.predict(embeddings)
 
-    # 释放设备资源，销毁窗口
-    cap.release()
-    cv2.destroyAllWindows()
+                    # 获得预测姓名和距离
+                    _, dis = self._recognize(embeddings)
+                    # 如果距离过大则认为识别失败
+                    if dis > 0.9:
+                        # 框出人脸位置并写上名字
+                        draw.rectangle((x1, y1, x2, y2), outline=(0, 0, 255), width=2)
+                        draw.text((x1, y1 - 40), f'未知', font=self.font, fill=(0, 0, 255))
+                        # cv2.putText(image, 'unknown', (x1 - 20, y1 - 20), cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
+                    else:
+                        draw.rectangle((x1, y1, x2, y2), outline=(0, 255, 0), width=2)
+                        draw.text((x1, y1 - 40), f'{name_knn[0]}({round(dis, 2)})', font=self.font, fill=(0, 255, 0))
+                        # cv2.putText(image, f'{name_knn[0]}({round(dis, 2)})', (x1 - 20, y1 - 20),
+                        #             cv2.FONT_ITALIC, 1, (255, 0, 255), 4)
+
+            # 显示处理后的图片
+            cv2.imshow(windows_name, np.array(img))
+
+            # 保持窗口
+            key = cv2.waitKey(1)
+            if key & 0xff == 27:
+                break
+
+        # 释放设备资源，销毁窗口
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    test()
+    try:
+        fr = FaceRecognizer()
+        fr.start_recognize()
+    except Exception as e:
+        print(e)
--- 模型训练.py
+++ 模型训练.py
@@ -24,45 +24,53 @@ def collate_fn(x):
     return x[0]
 
 
-dataset = datasets.ImageFolder('faces')
-dataset.idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
-loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=0, batch_size=20, drop_last=True)
+def train(path):
+    dataset = datasets.ImageFolder(path)
+    dataset.idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
+    loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=0, batch_size=2)
 
-print(dataset.idx_to_class)
+    print(dataset.idx_to_class)
 
-aligned = []
-names = []
-i = 0
-for x, y in loader:
-    try:
-        x_aligned, prob = mtcnn(x, return_prob=True)
-        if x_aligned is not None:
-            # print('Face detected with probability: {:8f}'.format(prob))
-            print(f'batch {i}')
-            i += 1
-            aligned.append(x_aligned)
-            # print(type(x_aligned))
-            # print(x_aligned)
-            names.append(dataset.idx_to_class[y])
-    except Exception as e:
-        print(e)
+    aligned = []
+    names = []
+    i = 0
+    for x, y in loader:
+        try:
+            x_aligned, prob = mtcnn(x, return_prob=True)
+            if x_aligned is not None:
+                # print('Face detected with probability: {:8f}'.format(prob))
+                print(f'batch {i}')
+                i += 1
+                aligned.append(x_aligned)
+                # print(type(x_aligned))
+                # print(x_aligned)
+                names.append(dataset.idx_to_class[y])
+        except Exception as e:
+            print(e)
+
+    aligned = torch.stack(aligned).to(device)
+    embeddings = resnet(aligned).detach().cpu()
+
+    a = pd.DataFrame(np.array(embeddings), index=names)
+    a.reset_index(inplace=True)
+    a.columns = ['label'] + [f'v{i}' for i in range(512)]
+
+    return a
 
 
-aligned = torch.stack(aligned).to(device)
-embeddings = resnet(aligned).detach().cpu()
+if __name__ == '__main__':
+    face_data = pd.concat([train(r'F:\Code\Python\人脸识别\计科四班人脸数据库\Part1'),
+                           train(r'F:\Code\Python\人脸识别\计科四班人脸数据库\Part2')])
 
-a = pd.DataFrame(np.array(embeddings), index=names)
-a.reset_index(inplace=True)
-a.columns = ['label'] + [f'v{i}' for i in range(512)]
-a.to_csv('face_data.csv', index=False)
+    face_data.to_csv('face_feature.csv', index=False, encoding='utf8')
 
-x = a.drop(columns=['label'])
-y = a['label']
-knn = KNN(n_neighbors=7)
-knn.fit(x, y)
+    x = face_data.drop(columns=['label'])
+    y = face_data['label']
+    knn = KNN(n_neighbors=5)
+    knn.fit(x, y)
 
-if os.path.exists('knn_model.pkl'):
-    os.remove('knn_model.pkl')
+    if os.path.exists('knn_model.pkl'):
+        os.remove('knn_model.pkl')
 
-joblib.dump(knn, 'knn_model.pkl')
-print('导出模型')
+    joblib.dump(knn, 'knn_model.pkl')
+    print('导出模型')
